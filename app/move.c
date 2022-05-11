f5a0ccc5d432d0d47976219101b20c9f6d9e553b
Anastasia <anastasia@treasure.PC>
2022-05-11 18:19:21 +0300

    Реализовала основные функции (до п.6 вкл.)

---
+++ app/move.c
@@ -0,0 +1,37 @@
+/**
+ * move.c -- реализует команду перемещения в заданную позицию 
+ */
+
+#include <stdio.h>
+#include "common.h"
+#include "text/_text.h"
+#include "text/text.h"
+
+void move(text txt, int line, int pose)
+{
+    node *current = txt->begin;
+    int k;
+    if (pose >= 0) {
+        txt->cursor->position = pose;
+    } else {
+        txt->cursor->position = 0;
+    }
+
+    if (line > 0) {
+        int l = txt->length;
+        if (line > l)
+            line = l;
+
+    while (current) {
+        if (k == line) {
+            txt->cursor->line = current;
+            break;
+        }
+        current = current->next;
+        k++;
+    }
+    }
+    else {
+        txt->cursor->line = current;
+    }
+}
